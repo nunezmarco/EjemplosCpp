b361c4d644b6c178cfbf256d5d6f9ebe49e0d6ba
nunezmarco <subodai1@gmail.com>
2018-05-14 16:01:04 +0000

    Pila

---
+++ Pila.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+template<typename Tipo>
+class Pila{
+    private:
+    int tope;
+    Tipo *arreglo;
+    int max;
+    
+    public:
+    Pila(int tam); //Constructor
+    ~Pila();       //Destructor
+    void Push(Tipo dato);
+    Tipo Pop();
+};
+
+template<typename Tipo>
+Pila<Tipo>::Pila(int tam){
+    max=tam;
+    tope=0;
+    arreglo=new Tipo[max];
+}
+
+template<typename Tipo>
+void Pila<Tipo>::Push(Tipo dato){
+    
+}
+
+template<typename Tipo>
+Tipo Pila<Tipo>::Pop(){
+    
+}
+
+template<typename Tipo>
+Pila<Tipo>::~Pila(){
+    delete []arreglo;
+}
+
+int main(){
+    Pila<int>miPila[5];
+    miPila.Push(3);
+    miPila.Push(7);
+    int x=miPila.Pop();
+    std::cout<< x << "\n";
+    x=miPila.Pop();
+    std::cout<< x << "\n";
+    x=miPila.Pop();
+    std::cout<< x << "\n";
+}
